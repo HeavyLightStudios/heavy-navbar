22f35af303cda4a4017d9e2c4326dd7ca5506489
Glenn Hevey <glenn.hevey@gmail.com>
2020-03-17 09:58:25 +1100

    Updated Dependencies

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,15 @@
 import {Config} from '@stencil/core';
+import {reactOutputTarget} from "@stencil/react-output-target";
 
 export const config: Config = {
   namespace: 'heavynavbar',
   globalStyle: 'src/globals/global.css',
   outputTargets: [
+    reactOutputTarget({
+      loaderDir: 'dist/loader',
+      componentCorePackage: 'heavy-navbar',
+      proxiesFile: '../heavy-navbar-react/src/components.ts',
+    }),
     {
       type: 'dist'
     },
